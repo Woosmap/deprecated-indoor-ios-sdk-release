32c3bdadd323b6a7e305b6eaf1c9529ff89b5dd9
sameer dhulap <sameer.dhulap@sanginfo.com>
2018-02-08 18:32:51 +0530

    only mapping sdk

--- InTripper.framework/Headers/Intripper.h
+++ InTripper.framework/Headers/Intripper.h
@@ -7,25 +7,7 @@
 //
 
 #import <InTripper/IntripperMap.h>
-#import <InTripper/IntripperAPI.h>
 #import <InTripper/IntripperMapService.h>
-#import <InTripper/IndoorMapObject.h>
-#import <InTripper/PathFormatter.h>
 #import <InTripper/MapSearch.h>
-#import <InTripper/THLabel.h>
-#import <InTripper/TrackingMarker.h>
-#import <InTripper/Preferences.h>
-#import <InTripper/NotificationEngine.h>
-#import <InTripper/LastKnownLocation.h>
-#import <InTripper/InAppNotification.h>
-
-#import <InTripper/AlphaCacheManager.h>
-#import <InTripper/AlphaEnvironment.h>
-#import <InTripper/IndoorMapProperties.h>
-#import <InTripper/GoogleFloorButton.h>
-#import <InTripper/blackInfoWindow.h>
-#import <InTripper/IntripperLanguage.h>
-#import <InTripper/AnimatingTrainMarker.h>
-#import <InTripper/TrackingAreaMarker.h>
 
 
